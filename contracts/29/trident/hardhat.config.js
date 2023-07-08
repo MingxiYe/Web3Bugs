ff876995bd579d0f8e7d53e50dfcd384706ecf5b
yolk-pie-L <493577282@qq.com>
2023-07-08 16:49:46 +0800

    successfully compiled 29

---
+++ contracts/29/trident/hardhat.config.js
@@ -0,0 +1,34 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 99999,
+          },
+        },
+      },
+      {
+        version: "0.4.19",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+};
