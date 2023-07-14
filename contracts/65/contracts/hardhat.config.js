de0cea86f9542cb88dfc543ee3a0e1c9759b1bf9
yolk-pie-L <493577282@qq.com>
2023-07-15 01:55:08 +0800

    fix: 65

---
+++ contracts/65/contracts/hardhat.config.js
@@ -0,0 +1,16 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1
+          }
+        }
+      },    
+    ]
+  },
+};
