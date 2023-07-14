8c28c8241d1a38608d7a505cd139c21c7c4154cc
yolk-pie-L <493577282@qq.com>
2023-07-14 12:57:51 +0800

    fix: 42

---
+++ contracts/42/projects/mochi-library/hardhat.config.js
@@ -0,0 +1,22 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.7',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+          metadata: {
+            // do not include the metadata hash, since this is machine dependent
+            // and we want all generated code to be deterministic
+            // https://docs.soliditylang.org/en/v0.8.4/metadata.html
+            bytecodeHash: 'none',
+          },
+        },
+      },
+    ],
+  },
+};
