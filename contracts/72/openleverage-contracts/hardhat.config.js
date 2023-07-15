5fda756b3ac5021ca9b6d0a8ed10aab7ea5cf8aa
yolk-pie-L <493577282@qq.com>
2023-07-15 10:23:44 +0800

    compile 72

---
+++ contracts/72/openleverage-contracts/hardhat.config.js
@@ -0,0 +1,12 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity:{
+  version:  "0.7.6",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 2000,
+    }
+  }
+}
+};
