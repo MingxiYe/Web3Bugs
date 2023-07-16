0f7674ae8b4eb9d9a27625a4ede1e0c9d858bbd6
yolk-pie-L <493577282@qq.com>
2023-07-16 23:27:57 +0800

    compiled 89

--- contracts/89/hardhat.config.js
+++ contracts/89/hardhat.config.js
@@ -1,23 +1,4 @@
-require("@nomiclabs/hardhat-waffle");
-require('@nomiclabs/hardhat-web3')
-require('hardhat-spdx-license-identifier')
-require('hardhat-docgen')
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.9",
-    networks: {
-        local: {
-            url: 'http://localhost:8545',
-            chainId: 1337
-        }
-    },
-    spdxLicenseIdentifier: {
-        runOnCompile: true
-    },
-    docgen: {
-        clear: true,
-    }
+  solidity: "0.8.9",
 };
