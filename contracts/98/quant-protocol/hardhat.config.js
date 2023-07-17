25c3416ca51e848d013bc666de979420a8c53573
yolk-pie-L <493577282@qq.com>
2023-07-17 22:26:07 +0800

    compiled 98

---
+++ contracts/98/quant-protocol/hardhat.config.js
@@ -0,0 +1,21 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      metadata: {
+        // do not include the metadata hash, since this is machine dependent
+        // https://github.com/paulrberg/solidity-template/issues/31
+        bytecodeHash: "none",
+      },
+      // You should disable the optimizer when debugging
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
+};
